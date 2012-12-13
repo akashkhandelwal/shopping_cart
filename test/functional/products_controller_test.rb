4e780a7578c24d4da53c3a6ebdcf8102303fcaa8
Akash Khandelwal <er.akashkhandelwal@gmail.com>
2012-12-13 18:39:10 +0530

    Product model unit test

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -6,10 +6,10 @@ class ProductsControllerTest < ActionController::TestCase
     @product = products(:one)
 
     @update = {
-      title: 'Lorem Ipsum',
+      title:       'Lorem Ipsum',
       description: 'Wibbles are fun!',
-      image_url: 'lorem.jpg',
-      price: 19.95
+      image_url:   'lorem.jpg',
+      price:        19.95
     }
 
   end
